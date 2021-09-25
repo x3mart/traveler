0e736e4516dab61a5d4652993ebe01a0ca32e770
moskvicheveg <moskvicheveg@gmail.com>
2021-09-25 13:24:51 +0400

    layout for project

--- frontend/components/P/P.props.ts
+++ frontend/components/P/P.props.ts
@@ -2,5 +2,7 @@ import { DetailedHTMLProps, HTMLAttributes, ReactNode } from "react";
 
 export interface PProps extends DetailedHTMLProps<HTMLAttributes<HTMLParagraphElement>, HTMLParagraphElement> {
     color?: 'p_footer';
+    link?: 'p_footer_link' | 'p_footer_link_col_2' | 'p_footer_letter_spacing' | 'p_footer_letter_spacing_margin_top';
     children: ReactNode;
+    href?: string;
 }
