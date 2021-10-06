2a8e9bc0ee583128ba77559f0ab63f571f1e9faf
moskvicheveg <moskvicheveg@gmail.com>
2021-10-06 11:46:43 +0400

    fix styles

--- frontend/components/CardTour/CardTour.props.ts
+++ frontend/components/CardTour/CardTour.props.ts
@@ -1,6 +1,6 @@
 import { DetailedHTMLProps, HTMLAttributes, ReactNode } from "react";
 
 export interface CardTourProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
-    block_style?: 'card_tour';
+    block_style?: 'card_tour' | 'card_tour_border';
     // children: ReactNode;   
 }
