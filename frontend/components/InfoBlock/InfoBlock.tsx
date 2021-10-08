b79fb108417f0886fd46b27cc8ecd4da5ca88f87
moskvicheveg <moskvicheveg@gmail.com>
2021-10-08 11:55:27 +0400

    mibile responsive

--- frontend/components/InfoBlock/InfoBlock.tsx
+++ frontend/components/InfoBlock/InfoBlock.tsx
@@ -2,13 +2,14 @@ import { InfoBlockProps } from "./InfoBlock.props";
 import styles from './InfoBlock.module.css';
 import cn from 'classnames';
 
-export const InfoBlock = ({ border_color, children, ...props }: InfoBlockProps): JSX.Element => {
+export const InfoBlock = ({ border_color, height_block, children, ...props }: InfoBlockProps): JSX.Element => {
   return (
     <div
         className={ cn(styles.infoblock, className, {
             [styles.orange_left_border]: border_color == 'orange_left_border',
             [styles.blue_left_border]: border_color == 'blue_left_border',
             [styles.white_left_border]: border_color == 'white_left_border',
+            [styles.travel_page]: height_block == 'travel_page',
         })}
         {...props}
     >  
