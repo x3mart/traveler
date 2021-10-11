c6d86d086b8a33ac10150f12daa96167d1972608
x3mart <x3mat@gmail.com>
2021-10-11 13:56:16 +0300

    TourAdvanced TeamMember  admin

--- tours/admin.py
+++ tours/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from tours.models import PropertyImage, PropertyType, TourAdvanced, TourBasic, TourDay, TourDayImage, TourImage, TourType
+
 # Register your models here.
+admin.site.register(TourBasic)
+admin.site.register(TourAdvanced)
+admin.site.register(TourType)
+admin.site.register(PropertyType)
+admin.site.register(PropertyImage)
+admin.site.register(TourImage)
+admin.site.register(TourDay)
+admin.site.register(TourDayImage)
