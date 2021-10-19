0fa2f60fd5c8d262b009a1728349dd6eb87e1b38
moskvicheveg <moskvicheveg@gmail.com>
2021-10-19 21:47:48 +0400

    added drafts foulder

--- frontend/components/TypeTourPage/TypeTourPage.props.ts
+++
@@ -1,6 +0,0 @@
-import { DetailedHTMLProps, HTMLAttributes, ReactNode } from "react";
-
-export interface TypeTourPageProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
-    // block_style?: 'presentation_block' | 'presentation_block_another';
-    children: ReactNode;   
-}
