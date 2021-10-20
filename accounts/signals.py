8396deb39d4199c240b8dc8aba8e831de82d9c01
x3mart <x3mat@gmail.com>
2021-10-20 12:52:21 +0300

    experts, users, team_members avatar processing

--- accounts/signals.py
+++ accounts/signals.py
@@ -5,14 +5,7 @@ from utils.images import crop_image, make_tmb
 import os
 from traveler.settings import BASE_DIR
 
-
-@receiver(post_init, sender=Expert)
-def backup_image_path(instance, **kwargs):
-    instance._current_avatar = instance.avatar
-    instance._current_tmb_avatar = instance.tmb_avatar
-
-@receiver(post_save, sender=Expert)
-def expert_post_save(instance, **kwargs):
+def avatar_processing(instance):
     crop_image(instance.avatar.path, 255, 355)
     make_tmb(instance.avatar.path)
     if instance._current_avatar != instance.avatar:
@@ -21,3 +14,22 @@ def expert_post_save(instance, **kwargs):
             storage.delete(instance._current_avatar.name)
         if os.path.exists(f'{BASE_DIR}{instance._current_tmb_avatar}'):
             os.remove(f'{BASE_DIR}{instance._current_tmb_avatar}')
+
+
+@receiver(post_init, sender=User)
+def backup_image_path(instance, **kwargs):
+    instance._current_avatar = instance.avatar
+    instance._current_tmb_avatar = instance.tmb_avatar
+
+@receiver(post_save, sender=User)
+def expert_post_save(instance, **kwargs):
+    avatar_processing(instance)
+
+@receiver(post_init, sender=Expert)
+def backup_image_path(instance, **kwargs):
+    instance._current_avatar = instance.avatar
+    instance._current_tmb_avatar = instance.tmb_avatar
+
+@receiver(post_save, sender=Expert)
+def expert_post_save(instance, **kwargs):
+    avatar_processing(instance)
