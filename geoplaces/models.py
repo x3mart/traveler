b3d278c0218bc584c5c8005cdafb73cfb22b4116
x3mart <x3mat@gmail.com>
2022-02-13 19:23:49 +0300

    geoplaces fix

--- geoplaces/models.py
+++ geoplaces/models.py
@@ -40,7 +40,7 @@ class City(models.Model):
     name = models.CharField(_('Название'), max_length=255)
     foreign_id = models.IntegerField(_('Сторонний ключ'), null=True, blank=True)
     country = models.ForeignKey('Country', on_delete=models.CASCADE, related_name='cities', verbose_name=_('Страна'), null=True, blank=True)
-    russian_region = models.ForeignKey('RussianRegion', on_delete=models.CASCADE, related_name='cities', verbose_name=_('Страна'), null=True, blank=True)
+    russian_region = models.ForeignKey('RussianRegion', on_delete=models.CASCADE, related_name='cities', verbose_name=_('Регион России'), null=True, blank=True)
     image = models.ImageField(_("Фото"), upload_to=geo_path, max_length=255, null=True, blank=True)
     alt =  models.CharField(_('alt текст'), max_length=255, null=True, blank=True)
 
