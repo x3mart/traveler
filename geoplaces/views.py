fe7567182c44e0bcd84000d2463cd5f1586a61bf
x3mart <x3mat@gmail.com>
2022-02-13 18:50:42 +0300

    geoplaces fix

--- geoplaces/views.py
+++ geoplaces/views.py
@@ -10,11 +10,30 @@ class RegionViewSet(viewsets.ModelViewSet):
     serializer_class = RegionSerializer
     permission_classes = [AllowAny]
 
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            data = serializer.validated_data
+        else:
+            return Response(serializer.errors, status=400)
+        instance, created = Region.objects.get_or_create(**data)
+        return Response(RegionSerializer(instance).data, status=201)
+
 
 class CountryViewSet(viewsets.ModelViewSet):
     queryset = Country.objects.all()
     serializer_class = CountrySerializer
     permission_classes = [AllowAny]
+
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            data = serializer.validated_data
+        else:
+            return Response(serializer.errors, status=400)
+        instance, created = Country.objects.get_or_create(**data)
+        return Response(CountrySerializer(instance).data, status=201)
 
 
 class RussianRegionViewSet(viewsets.ModelViewSet):
@@ -23,7 +42,26 @@ class RussianRegionViewSet(viewsets.ModelViewSet):
     permission_classes = [AllowAny]
 
 
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            data = serializer.validated_data
+        else:
+            return Response(serializer.errors, status=400)
+        instance, created = RussianRegion.objects.get_or_create(**data)
+        return Response(RussianRegionSerializer(instance).data, status=201)
+
+
 class CityViewSet(viewsets.ModelViewSet):
     queryset = City.objects.all()
     serializer_class = CitySerializer
     permission_classes = [AllowAny]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            data = serializer.validated_data
+        else:
+            return Response(serializer.errors, status=400)
+        instance, created = City.objects.get_or_create(**data)
+        return Response(CitySerializer(instance).data, status=201)
