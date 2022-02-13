bf4ab64edc511d09981c6c59467024e80b1115e3
x3mart <x3mat@gmail.com>
2022-02-13 16:03:19 +0300

    tours urls tourdays & tourdayimages added

--- tours/urls.py
+++ tours/urls.py
@@ -1,9 +1,11 @@
 from rest_framework.routers import DefaultRouter
-from .views import TourViewSet, TourBasicViewSet, TourTypeViewSet
+from .views import TourViewSet, TourBasicViewSet, TourTypeViewSet, TourDayViewSet, TourDayImageViewSet
 
 router = DefaultRouter()
 router.register(r'tours', TourViewSet, basename='tour')
 router.register(r'basictours', TourBasicViewSet, basename='basictour')
 router.register(r'tourtypes', TourTypeViewSet, basename='tourtype')
+router.register(r'tourdays', TourDayViewSet, basename='tourday')
+router.register(r'tourdayimages', TourDayImageViewSet, basename='tourdayimage')
 
 urlpatterns = router.urls
