b32bd32330352e4fcf98c4e0c129a7640e0c3523
x3mart <x3mat@gmail.com>
2022-02-13 19:10:00 +0300

    geoplaces fix

--- geoplaces/models.py
+++ geoplaces/models.py
@@ -24,6 +24,7 @@ class Region(models.Model):
 
 class Country(models.Model):
     name = models.CharField(_('Название'), max_length=255)
+    foreign_id = models.IntegerField(_('Сторонний ключ'), null=True, blank=True)
     region = models.ForeignKey('Region', on_delete=models.CASCADE, related_name='countries', verbose_name=_('Регион'),)
     image = models.ImageField(_("Фото"), upload_to=geo_path, max_length=255, null=True, blank=True)
     alt =  models.CharField(_('alt текст'), max_length=255, null=True, blank=True)
@@ -37,6 +38,7 @@ class Country(models.Model):
 
 class City(models.Model):
     name = models.CharField(_('Название'), max_length=255)
+    foreign_id = models.IntegerField(_('Сторонний ключ'), null=True, blank=True)
     country = models.ForeignKey('Country', on_delete=models.CASCADE, related_name='cities', verbose_name=_('Страна'), null=True, blank=True)
     russian_region = models.ForeignKey('RussianRegion', on_delete=models.CASCADE, related_name='cities', verbose_name=_('Страна'), null=True, blank=True)
     image = models.ImageField(_("Фото"), upload_to=geo_path, max_length=255, null=True, blank=True)
@@ -51,6 +53,7 @@ class City(models.Model):
 
 class RussianRegion(models.Model):
     name = models.CharField(_('Название'), max_length=255)
+    foreign_id = models.IntegerField(_('Сторонний ключ'), null=True, blank=True)
     image = models.ImageField(_("Фото"), upload_to=geo_path, max_length=255, null=True, blank=True)
     alt =  models.CharField(_('alt текст'), max_length=255, null=True, blank=True)
 
