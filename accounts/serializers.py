d162c28efa73f57607a42c4b841863a2d86e6f7f
x3mart <x3mat@gmail.com>
2022-03-19 21:14:28 +0300

    expert password

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -93,7 +93,7 @@ class ExpertMeSerializer(serializers.ModelSerializer):
     tmb_avatar = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = Expert
-        fields = ('id','email', 'first_name', 'last_name', 'avatar', 'phone', 'tmb_avatar', 'country', 'city', 'languages', 'visited_countries', 'about', 'email_confirmed', 'phone_confirmed', 'docs_confirmed', 'status_confirmed', 'rating', 'tours_count', 'tours_rating', 'reviews_count', 'tour_reviews_count', 'video')
+        fields = ('id', 'password', 'email', 'first_name', 'last_name', 'avatar', 'phone', 'tmb_avatar', 'country', 'city', 'languages', 'visited_countries', 'about', 'email_confirmed', 'phone_confirmed', 'docs_confirmed', 'status_confirmed', 'rating', 'tours_count', 'tours_rating', 'reviews_count', 'tour_reviews_count', 'video')
         extra_kwargs = {
             'password': {'write_only': True, 'required': False,},
             'avatar': {'read_only': True, 'required': False,},
@@ -105,7 +105,7 @@ class ExpertMeSerializer(serializers.ModelSerializer):
     def update(self, instance, validated_data):
         validated_data['is_expert'] = True
         
-        if validated_data.get('password') is not None:
+        if validated_data.get('password'):
             # check_password(self)
             password = validated_data.pop('password')
             instance.set_password(password)
