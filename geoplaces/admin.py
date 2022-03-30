116599236f0ea4f5d1a25bbb41ee68a00532fd5b
x3mart <x3mat@gmail.com>
2022-03-30 18:43:08 +0300

    vk cities

--- geoplaces/admin.py
+++ geoplaces/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
-from geoplaces.models import City, Country, Region, CountryRegion, VKCity
+from geoplaces.models import City, Country, Region, CountryRegion
 
 # Register your models here.
 admin.site.register(Region)
 admin.site.register(Country)
 admin.site.register(CountryRegion)
 admin.site.register(City)
-admin.site.register(VKCity)
+# admin.site.register(VKCity)
