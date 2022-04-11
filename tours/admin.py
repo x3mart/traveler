4da69702ea114aac4bb9edabc2024d61571c1d02
x3mart <x3mat@gmail.com>
2022-04-11 14:48:26 +0300

    tour admin fix city

--- tours/admin.py
+++ tours/admin.py
@@ -3,7 +3,12 @@ from django.contrib import admin
 from tours.models import ImportantTitle, TourAccomodation, TourPropertyType, Tour, TourType
 
 # Register your models here.
-admin.site.register(Tour)
+class TourAdmin(admin.ModelAdmin):
+    readonly_fields=('start_city', 'finish_city')
+
+
+
+admin.site.register(Tour, TourAdmin)
 admin.site.register(TourType)
 admin.site.register(TourPropertyType)
 admin.site.register(TourAccomodation)
