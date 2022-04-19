7385d60f0160eef6c6cccdae2e5b28ac6502a8db
x3mart <x3mat@gmail.com>
2022-04-19 19:47:16 +0300

    support tg bot

--- tgbots/views.py
+++ tgbots/views.py
@@ -3,12 +3,25 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework import permissions
 import requests
+from django.template.loader import render_to_string
 from .models import *
 from .serializers import *
 from traveler.settings import TG_URL
 
 
 # Create your views here.
+COMMANDS_LIST = ('start',)
+
+def get_tg_account(user):
+    tg_account, created = TelegramAccount.objects.get_or_create(tg_id=user['id'])
+    if created:
+        user.pop('id')
+        tg_account = TelegramAccount.objects.filter(pk=tg_account.id)
+        tg_account.update(**user)
+        tg_account = tg_account.first()
+    return tg_account
+
+
 @api_view(["POST", "GET"])
 @permission_classes((permissions.AllowAny,))
 def tg_update_handler(request):
@@ -17,8 +30,7 @@ def tg_update_handler(request):
         update = Update(request.data)
         if hasattr(update,'message'):
             response = SendMessage(chat_id=update.get_chat(), text=update.get_chat()).send()
-            # response2 = SendMessage(chat_id=update.get_chat(), text=response).send()
-            # update.message_dispatcher()
+            update.message_dispatcher()
         elif hasattr(update,'callback_query'):
             update.callback_dispatcher()
         # method = "sendMessage"
@@ -26,7 +38,7 @@ def tg_update_handler(request):
         # data = SendMessageSerializer(send_message).data
         # requests.post(TG_URL + method, data)
     except:
-       response2 = SendMessage(chat_id=update.get_chat(), text=response).send()
+       response2 = SendMessage(chat_id=1045490278, text='error').send()
     return Response({}, status=200)
 
 class Update():
@@ -39,14 +51,75 @@ class Update():
                 self.__setattr__(key, CallbackQuery(value))
             else:
                 self.__setattr__(key, value)
+
+    def message_dispatcher(self):
+        command, args = self.command_handler(self.message.text)
+        self.tg_account = get_tg_account(self.message.user)
+        if self.tg_account.await_reply:
+            response = self.await_despatcher(self.message.text, command, args)
+        elif command:
+            # response = SendMessage(chat_id=1045490278, text=command).send()
+            response = self.command_dispatcher('message', command, args)
+        else:
+            text = "No commands in message"
+            # response = SendMessage(chat_id=self.message.chat.id, text=text).send()
+            response = None
+        return response
     
+    def callback_dispatcher(self):
+        command, args = self.command_handler(self.callback_query.data)
+        self.tg_account = get_tg_account(self.callback_query.user)
+        response = self.callback_query.answer()
+        if command:
+            response = self.command_dispatcher('callback_query', command, args)
+        else:
+            text = "No commands in callback_query"
+            # response = SendMessage(chat_id=self.message.chat.id, text=text).send()
+            response = None
+        return response
+    
+    def command_handler(self, text):
+        command = ''
+        if text.startswith('/'):
+            command_message = text.split(' ')
+            command = command_message.pop(0).replace('/', '')
+        if command in COMMANDS_LIST:
+                return (command, command_message)
+        else:
+            return (None, [])
+
     def get_chat(self, source=None):
         if hasattr(self, 'callback_query'):
-            print(self.callback_query)
             chat_id=self.callback_query.message.chat.id
         else:
             chat_id=self.message.chat.id
         return chat_id
+    
+    def get_message(self,source=None):
+        if hasattr(self, 'callback_query'):
+            message=self.callback_query.message
+        else:
+            message=self.message
+        return message
+    
+    def command_dispatcher(self, command, args=[]):
+        chat_id = self.get_chat()
+        message = self.get_message()
+        kwargs = {}
+        if command == 'start':
+            if self.tg_account.account:
+                text = render_to_string('start_for_auth.html', {'account': self.tg_account.account})
+            else:
+                text = render_to_string('start.html', {})
+            reply_markup = ReplyMarkup().get_markup(command, self.tg_account)
+            response = SendMessage(chat_id, text, reply_markup).send()
+        else:
+            response = None
+        return response
+    
+    def await_despatcher(self, text, command=None, args=None):
+        chat_id = self.get_chat()
+        message = self.get_message()
 
 
 class CallbackQuery():
@@ -82,3 +155,24 @@ class SendMessage():
         data = SendMessageSerializer(self).data
         response = requests.post(TG_URL + 'sendMessage', data)
         return response
+
+
+class ReplyMarkup():
+    def __init__(self):
+        pass
+
+    def get_markup(self, name, tg_account=None, **kwargs):
+        if name == 'start' and tg_account and not tg_account.account and hasattr(tg_account.account, 'expert') and tg_account.account.expert.phone_confirmed:
+            button1 = InlineButton(text='Создать заявку', callback_data=f'/create_ticket')
+            keyboard = [[button1]]
+        elif name == 'start' and tg_account and tg_account.account and hasattr(tg_account.account, 'expert') and not tg_account.account.expert.phone_confirmed:
+            button1 = InlineButton(text='Подтвердить телефон', callback_data=f'/comfirm_phone')
+            button2 = InlineButton(text='Создать заявку', callback_data=f'/create_ticket')
+            keyboard = [[button1],[button2]]
+        else:
+            button1 = InlineButton(text='Авторизация', callback_data=f'/login')
+            keyboard = [[button1]]
+        
+        self.inline_keyboard = keyboard
+        reply_markup_data = ReplyMarkupSerializer(self).data
+        return JSONRenderer().render(reply_markup_data)
