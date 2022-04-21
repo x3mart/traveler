5388607703271892981dbba33d9aaaa9b4dd1f59
x3mart <x3mat@gmail.com>
2022-04-21 17:44:39 +0300

    support tg bot

--- tgbots/views.py
+++ tgbots/views.py
@@ -30,19 +30,19 @@ def get_tg_account(user):
 @permission_classes((permissions.AllowAny,))
 def tg_update_handler(request):
     # response = SendMessage(chat_id=1045490278, text='update').send()
-    # try:
-    update = Update(request.data)
-    if hasattr(update,'message'):
-        response = SendMessage(chat_id=1045490278, text=request.data).send()
-        response = update.message_dispatcher()
-    elif hasattr(update,'callback_query'):
-        update.callback_dispatcher()
+    try:
+        update = Update(request.data)
+        if hasattr(update,'message'):
+            response = SendMessage(chat_id=1045490278, text=request.data).send()
+            response = update.message_dispatcher()
+        elif hasattr(update,'callback_query'):
+            update.callback_dispatcher()
         # method = "sendMessage"
         # send_message = SendMessage(chat_id=1045490278, text=f'{request.data}')
         # data = SendMessageSerializer(send_message).data
         # requests.post(TG_URL + method, data)
-    # except:
-    #    response2 = SendMessage(chat_id=1045490278, text='response').send()
+    except:
+       response2 = SendMessage(chat_id=1045490278, text='response').send()
     return Response({}, status=200)
 
 class Update():
@@ -192,12 +192,17 @@ class Update():
             response = SendMessage(self.message.chat.id, text, reply_markup).send()
         elif self.tg_account.reply_type =='create_ticket':
             ticket = Ticket.objects.create(user=self.tg_account.account.expert, tg_chat=chat_id)
-            message = SupportChatMessage.objects.create(sender=self.tg_account.account.expert, tg_message=message.message_id, sender_chat_id=chat_id, text=message.text)
-            self.tg_account.reply_type =='ticket'
+            message = SupportChatMessage.objects.create(sender=self.tg_account.account.expert, tg_message=message.message_id, sender_chat_id=chat_id, text=message.text, ticket=ticket)
+            self.tg_account.reply_type ='ticket'
+            self.tg_account.save()
             text = render_to_string('ticket_created.html', {'ticket':ticket})
             response = SendMessage(chat_id, text).send()
+        elif self.tg_account.reply_type =='ticket':
+            ticket = Ticket.objects.filter(user_id=self.tg_account.account_id).order_by('-id').first()
+            message = SupportChatMessage.objects.create(sender=self.tg_account.account.expert, tg_message=message.message_id, sender_chat_id=chat_id, text=message.text, ticket_id=ticket.id)
+            response = None
         else:
-            response = self.command_dispatcher('message', command, args) if command else None 
+            response = self.command_dispatcher(command, args) if command else None 
             self.tg_account.await_reply = False
             self.tg_account.reply_1 = None
             self.tg_account.reply_type = None
