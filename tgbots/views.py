21cf638a6559d333ad0e388439383c40341237b5
x3mart <x3mat@gmail.com>
2022-04-21 19:31:39 +0300

    support tg bot

--- tgbots/views.py
+++ tgbots/views.py
@@ -153,7 +153,7 @@ class Update():
             self.tg_account.await_reply = False
             self.tg_account.reply_type = None
             self.tg_account.save()
-            ticket = Ticket.objects.filter(user_id=self.tg_account.account_id).filter(status_in=[1,2]).order_by('-id').first()
+            ticket = Ticket.objects.filter(user_id=self.tg_account.account_id).filter(status__in=[1,2]).order_by('-id').first()
             reply_markup = ReplyMarkup().get_markup('start', self.tg_account)
             response = SendMessage(chat_id, 'Заявка закрыта', reply_markup).send()
         else:
@@ -205,7 +205,7 @@ class Update():
             text = render_to_string('ticket_created.html', {'ticket':ticket})
             response = SendMessage(chat_id, text).send()
         elif self.tg_account.reply_type =='ticket':
-            ticket = Ticket.objects.filter(user_id=self.tg_account.account_id).filter(status_in=[1,2]).order_by('-id').first()
+            ticket = Ticket.objects.filter(user_id=self.tg_account.account_id).filter(status__in=[1,2]).order_by('-id').first()
             message = SupportChatMessage.objects.create(sender=self.tg_account.account.expert, tg_message=message.message_id, sender_chat_id=chat_id, text=message.text, ticket_id=ticket.id)
             response = None
         else:
