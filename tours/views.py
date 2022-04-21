f031abcc350c271a93294503c3196188b39e9e68
x3mart <x3mat@gmail.com>
2022-04-21 21:46:58 +0300

    tour list

--- tours/views.py
+++ tours/views.py
@@ -30,7 +30,7 @@ class TourViewSet(viewsets.ModelViewSet, TourMixin):
         if self.action in ['list',]:
             tour_basic = TourBasic.objects.prefetch_related('expert')
             prefetchet_tour_basic = Prefetch('tour_basic', tour_basic)
-            qs = Tour.objects.prefetch_related(prefetchet_tour_basic, 'start_country', 'start_city', 'wallpaper', 'curency').only('id', 'name', 'start_date', 'start_country', 'start_city', 'price', 'discount', 'duration', 'tour_basic', 'wallpaper').filter(is_active=True).filter(tour_basic__direct_link=False).filter(start_date__gte=datetime.today())
+            qs = Tour.objects.prefetch_related(prefetchet_tour_basic, 'start_country', 'start_city', 'wallpaper', 'currency').only('id', 'name', 'start_date', 'start_country', 'start_city', 'price', 'discount', 'duration', 'tour_basic', 'wallpaper').filter(is_active=True).filter(tour_basic__direct_link=False).filter(start_date__gte=datetime.today())
         elif self.action in ['tour_set',]:
             qs = Tour.objects.prefetch_related('tour_basic', 'start_country', 'currency').only('id', 'name', 'start_date', 'finish_date', 'start_country', 'price', 'cost', 'discount', 'on_moderation', 'is_active', 'is_draft', 'duration', 'sold', 'watched', 'currency', 'tour_basic', 'wallpaper').filter(tour_basic__expert_id=self.request.user.id).order_by('-id')
         else:
