8d6df76e208f7701facbd5bdf8ae285ca18c2e22
x3mart <x3mat@gmail.com>
2022-04-25 18:30:11 +0300

    tour fix

--- tours/views.py
+++ tours/views.py
@@ -31,8 +31,8 @@ class TourViewSet(viewsets.ModelViewSet, TourMixin):
     def get_queryset(self):
         if self.action in ['list',]:
             tour_basic = TourBasic.objects.prefetch_related('expert')
-            prefetchet_tour_basic = Prefetch('tour_basic', tour_basic)
-            qs = Tour.objects.prefetch_related(prefetchet_tour_basic, 'start_country', 'start_city', 'wallpaper', 'currency').only('id', 'name', 'start_date', 'start_country', 'start_city', 'price', 'discount', 'duration', 'tour_basic', 'wallpaper', 'vacants_number', 'currency').filter(is_active=True).filter(tour_basic__direct_link=False).filter(Q(booking_delay__lte=F('start_date') - datetime.today().date() - F('postpay_days_before_start')))
+            prefetch_tour_basic = Prefetch('tour_basic', tour_basic)
+            qs = Tour.objects.prefetch_related(prefetch_tour_basic, 'start_country', 'start_city', 'wallpaper', 'currency').only('id', 'name', 'start_date', 'start_country', 'start_city', 'price', 'discount', 'duration', 'tour_basic', 'wallpaper', 'vacants_number', 'currency').filter(is_active=True).filter(tour_basic__direct_link=False).filter(Q(booking_delay__lte=F('start_date') - datetime.today().date() - F('postpay_days_before_start')))
         elif self.action in ['tour_set',]:
             qs = Tour.objects.prefetch_related('tour_basic', 'start_country', 'currency').only('id', 'name', 'start_date', 'finish_date', 'start_country', 'price', 'cost', 'discount', 'on_moderation', 'is_active', 'is_draft', 'duration', 'sold', 'watched', 'currency', 'tour_basic', 'wallpaper').filter(tour_basic__expert_id=self.request.user.id).order_by('-id')
         else:
