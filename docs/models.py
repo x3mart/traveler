f65dde68db96dfbb33ac5ae02d36aacd6f05df62
x3mart <x3mat@gmail.com>
2022-04-26 14:50:19 +0300

    legals

--- docs/models.py
+++ docs/models.py
@@ -7,4 +7,4 @@ class LegalDocument(models.Model):
     docs_title = models.CharField(_('Заголовок'), max_length=255,  null=True, blank=True,)
     docs_subtitle = models.CharField(_('Подзаголовок'), max_length=255,  null=True, blank=True,)
     docs_meta_tag = models.TextField(_('Мета теги'), max_length=255,  null=True, blank=True,)
-    docs_body = RichTextField(_('Текст'), max_length=255,  null=True, blank=True,)
+    docs_body = RichTextField(_('Текст'), null=True, blank=True,)
