389cad6aae3ba21adbe507a98d87e0771dba4389
x3mart <x3mat@gmail.com>
2022-04-29 16:52:39 +0300

    verification

--- verificationrequests/serializers.py
+++ verificationrequests/serializers.py
@@ -1,14 +1,20 @@
 from rest_framework import serializers
+
+from geoplaces.serializers import CountrySerializer
 from .models import Legal, Individual
 
 
 class LegalSerializer(serializers.ModelSerializer):
+    residency = CountrySerializer(many=False, read_only=True)
+    tours_countries = CountrySerializer(many=True, read_only=True)
     class Meta:
         model = Legal
         exclude = ('expert', 'aproved')
 
 
 class IndividualSerializer(serializers.ModelSerializer):
+    residency = CountrySerializer(many=False, read_only=True)
+    tours_countries = CountrySerializer(many=True, read_only=True)
     class Meta:
         model = Individual
         exclude = ('expert', 'aproved')
