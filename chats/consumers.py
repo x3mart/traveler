b9ffe293c040844e4574f0e4169b751d1eb1ae55
x3mart <x3mat@gmail.com>
2022-05-20 19:42:27 +0300

    chat

--- chats/consumers.py
+++ chats/consumers.py
@@ -9,33 +9,33 @@ from chats.serializers import ChatMessageSerializer
 
 class ChatConsumer(AsyncWebsocketConsumer):
 
-    # @database_sync_to_async
-    # def get_chat(self):
-    #     return UserChat.objects.get(pk=self.room_name)
+    @database_sync_to_async
+    def get_chat(self):
+        return UserChat.objects.get(pk=self.room_name)
     
-    # @database_sync_to_async
-    # def get_old_messages(self):
-    #     messages = ChatMessage.objects.filter(room=self.room_name)
-    #     return ChatMessageSerializer(messages, many=True).data
+    @database_sync_to_async
+    def get_old_messages(self):
+        messages = ChatMessage.objects.filter(room=self.room_name)
+        return ChatMessageSerializer(messages, many=True).data
     
-    # @database_sync_to_async
-    # def save_message(self, message):
-    #     ChatMessage.objects.create(room=self.chat, author=self.user, text=message)
+    @database_sync_to_async
+    def save_message(self, message):
+        ChatMessage.objects.create(room=self.chat, author=self.user, text=message)
     
-    # @database_sync_to_async
-    # def set_online_status_member_in_room(self, online=False):
-    #     if online:
-    #         self.chat.members_in_room.add(self.user)
-    #     else:
-            # self.chat.members_in_room.remove(self.user)
+    @database_sync_to_async
+    def set_online_status_member_in_room(self, online=False):
+        if online:
+            self.chat.members_in_room.add(self.user)
+        else:
+            self.chat.members_in_room.remove(self.user)
     
 
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
-        # self.user = self.scope['user']
-        # self.chat = await self.get_chat()
-        # await self.set_online_status_member_in_room(online=True)
+        self.user = self.scope['user']
+        self.chat = await self.get_chat()
+        await self.set_online_status_member_in_room(online=True)
 
         # Join room group
         await self.channel_layer.group_add(
@@ -59,7 +59,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
         # }))
 
     async def disconnect(self, close_code):
-        # await self.set_online_status_member_in_room(online=False)
+        await self.set_online_status_member_in_room(online=False)
         # await self.channel_layer.group_send(
         #     'chat_notify',
         #     {
