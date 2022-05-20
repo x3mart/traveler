5f15917654c93db50d266a036799a71762f9cd52
x3mart <x3mat@gmail.com>
2022-05-20 14:19:50 +0300

    chat

--- traveler/asgi.py
+++ traveler/asgi.py
@@ -11,12 +11,12 @@ import os
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-import chats.routing
 # from chats.middelware import JwtAuthMiddlewareStack
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'traveler.settings')
 django_asgi_app = get_asgi_application()
 
+import chats.routing
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
