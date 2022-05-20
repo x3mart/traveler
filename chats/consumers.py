17d60a1a2f5e07b8c39538a483e3961c86142e86
x3mart <x3mat@gmail.com>
2022-05-20 22:04:15 +0300

    chat

--- chats/consumers.py
+++ chats/consumers.py
@@ -15,13 +15,13 @@ class ChatConsumer(AsyncWebsocketConsumer):
     
     @database_sync_to_async
     def get_old_messages(self):
-        messages = ChatMessage.objects.filter(room=self.room_name)
+        messages = ChatMessage.objects.filter(room=self.room_name).order_by('created_at')
         return ChatMessageSerializer(messages, many=True).data
     
     @database_sync_to_async
     def save_message(self, message):
         message = ChatMessage.objects.create(room=self.chat, author=self.user, text=message)
-        return message
+        return ChatMessageSerializer(message, many=False).data
     
     @database_sync_to_async
     def set_online_status_member_in_room(self, online=False):
@@ -37,7 +37,8 @@ class ChatConsumer(AsyncWebsocketConsumer):
         self.user = self.scope['user']
         self.chat = await self.get_chat()
         await self.set_online_status_member_in_room(online=True)
-        # self.old_messages = await self.get_old_messages()
+        self.old_messages = await self.get_old_messages()
+        print(json.dumps(self.old_messages))
 
         # Join room group
         await self.channel_layer.group_add(
@@ -54,10 +55,12 @@ class ChatConsumer(AsyncWebsocketConsumer):
         #         'message': 'Wellcome'
         #     }
         # )
-
-        await self.send(text_data=json.dumps({
-            'old_messages': 'self.old_messages'
-        }))
+        for message in self.old_messages:
+            await self.send(text_data=json.dumps({
+            'message': message['text'],
+            'created_at': message['created_at'],
+            'author': message['author']
+            }))
 
     async def disconnect(self, close_code):
         await self.set_online_status_member_in_room(online=False)
@@ -78,13 +81,12 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-        message2 = await self.save_message(message)
         # Send message to room group
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
-                'message': message
+                'message': message,
             }
         )
         
@@ -93,8 +95,10 @@ class ChatConsumer(AsyncWebsocketConsumer):
     # Receive message from room group
     async def chat_message(self, event):
         message = event['message']
-
+        message = await self.save_message(message)
         # Send message to WebSocket
         await self.send(text_data=json.dumps({
-            'message': message
+            'message': message['text'],
+            'created_at': message['created_at'],
+            'author': message['author']
         }))
