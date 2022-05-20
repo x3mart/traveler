2a1e5ed31f949abb48ab6853b54877578dd24490
x3mart <x3mat@gmail.com>
2022-05-20 17:40:30 +0300

    chat

--- chats/consumers.py
+++ chats/consumers.py
@@ -33,7 +33,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
-        self.user = self.scope['user']
+        # self.user = self.scope['user']
         # self.chat = await self.get_chat()
         # await self.set_online_status_member_in_room(online=True)
 
