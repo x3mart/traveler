bdaf0128b449c17ff2347f2aa45312a694c6bb3c
x3mart <x3mat@gmail.com>
2022-05-21 21:46:34 +0300

    article

--- articles/serializers.py
+++ articles/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
+from utils.images import get_tmb_image_uri
 from .models import Article
 
 class ArticleSerializer(serializers.ModelSerializer):
+    tmb_image = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Article
         fields = '__all__'
+    
+    def get_tmb_avatar(self, obj): 
+        return get_tmb_image_uri(self, obj) 
