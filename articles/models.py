5459109dd411515753acefd4f502e225d11ea57a
x3mart <x3mat@gmail.com>
2022-05-21 21:36:44 +0300

    article

--- articles/models.py
+++ articles/models.py
@@ -22,6 +22,9 @@ class BaseAbstractArticle(models.Model):
 
     class Meta():
         abstract = True
+    
+    def __str__(self):
+        return self.title
 
 
 class Article(BaseAbstractArticle):
