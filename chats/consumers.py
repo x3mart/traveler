965629066c3a3bb70116065308d11dc28f3f6ad0
x3mart <x3mat@gmail.com>
2022-05-23 20:20:18 +0300

    chats

--- chats/consumers.py
+++ chats/consumers.py
@@ -3,7 +3,7 @@ from channels.generic.websocket import AsyncWebsocketConsumer, WebsocketConsumer
 from channels.db import database_sync_to_async
 from asgiref.sync import sync_to_async
 from django.forms.models import model_to_dict
-# from accounts.models import User
+from accounts.models import User
 from chats.models import ChatMessage, UserChat
 from chats.serializers import ChatMessageSerializer
 
@@ -107,6 +107,63 @@ class ChatConsumer(AsyncWebsocketConsumer):
         
         
     # Receive message from room group
+    async def chat_message(self, event):
+        if event.get('message'):
+            message = event['message']
+            # Send message to WebSocket
+            await self.send(text_data=json.dumps({
+                'message': message['text'],
+                'created_at': message['created_at'],
+                'author': message['author'],
+                'is_read': message['is_read']
+            }))
+        elif event.get('command'):
+            # Send command to WebSocket
+            await self.send(text_data=json.dumps({
+                'command': event['command']
+            }))
+
+
+class NotificationConsumer(AsyncWebsocketConsumer):
+
+    @database_sync_to_async
+    def set_online_status(self, online=False):
+        User.objects.filter(pk=self.user.id).update(is_online=online)
+
+
+    async def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = 'chat_%s' % self.room_name
+        self.user = self.scope['user']
+        self.chat = await self.get_chat()
+
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+        await self.accept()
+        await self.set_online_status(online=True)
+    
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name       
+        )
+        await self.set_online_status(online=False)
+    
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message
+            }
+        )
+    
     async def chat_message(self, event):
         if event.get('message'):
             message = event['message']
