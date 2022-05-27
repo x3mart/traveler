f73ced352c94652c3b67ce9d00694003856d0677
x3mart <x3mat@gmail.com>
2022-05-27 15:32:08 +0300

    tours on moderation admin

--- tours/admin.py
+++ tours/admin.py
@@ -24,7 +24,8 @@ class ModeratedTourAdmin(admin.ModelAdmin):
     readonly_fields=('start_city', 'finish_city')
     list_display = ('name', 'expert', 'start_country', 'start_city', 'start_date', 'is_active', 'on_moderation', 'is_draft', 'direct_link')
     # list_editable =('is_active', 'on_moderation', 'is_draft')
-    list_filter = ('is_active', 'on_moderation', 'is_draft', 'tour_basic__expert')
+    list_filter = ('tour_basic__expert',)
+    list_display_links = None
 
     def get_queryset(self, request):
         qs = super().get_queryset(request)
