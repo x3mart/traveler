e8b4377b799777fc7743afec91941109ca68ec79
x3mart <x3mat@gmail.com>
2022-05-30 17:46:07 +0300

    chats admin

--- chats/models.py
+++ chats/models.py
@@ -22,5 +22,8 @@ class ChatMessage(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     is_read = models.BooleanField(default=False)
 
+    def __str__(self):
+        return f'{self.author} {self.created_at.strftime("%d.%m.%Y, %H:%M:%S")}'
+
     # class Meta:
     #     ordering = ['-created_at']
