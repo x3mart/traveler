2ad93a7ed9a347a51862a3b144819914d860f931
x3mart <x3mat@gmail.com>
2022-05-30 17:42:58 +0300

    chats admin

--- chats/admin.py
+++ chats/admin.py
@@ -17,7 +17,7 @@ class ChatMessageInline(admin.TabularInline):
 
 class UserChatAdmin(admin.ModelAdmin):
     list_display = ('__str__',)
-    fieldsets = ((None, {}),)
+    fieldsets = ((None, None),)
     inlines = [
         ChatMessageInline,
     ]
