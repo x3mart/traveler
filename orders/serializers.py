619120efacb255a62ac019a7fb0c27199ce0cca8
x3mart <x3mat@gmail.com>
2022-06-15 16:43:31 +0300

    orders

--- orders/serializers.py
+++ orders/serializers.py
@@ -50,7 +50,7 @@ class OrderSerializer(serializers.ModelSerializer):
             'travelers_number':{'required': False},
             'cost':{'read_only': True, 'required': False},
             'postpay_final_date':{'read_only': True, 'required': False},
-            'status':{'read_only': True, 'required': False},
+            'status':{'required': False},
             'difficulty_level':{'read_only': True, 'required': False},
             'comfort_level':{'read_only': True, 'required': False},
             'tour_excluded_services':{'read_only': True, 'required': False},
