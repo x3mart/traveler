9a1f02564c948b7b106c50342a39056d538cb3a0
x3mart <x3mat@gmail.com>
2022-06-18 15:15:59 +0300

    tour filterset

--- tours/views.py
+++ tours/views.py
@@ -237,5 +237,5 @@ class FilterView(APIView):
         qs = Tour.objects.prefetch_related(prefetch_tour_basic, 'start_country', 'start_city', 'wallpaper', 'currency').only('id', 'name', 'start_date', 'start_country', 'start_city', 'price', 'discount', 'duration', 'tour_basic', 'wallpaper', 'vacants_number', 'currency').filter(is_active=True).filter(direct_link=False).filter(Q(booking_delay__lte=F('start_date') - datetime.today().date() - F('postpay_days_before_start')))
         types_basic = qs.values_list('basic_type__name', flat=True).distinct()
         additional_types = qs.values_list('additional_types__name', flat=True).distinct()
-        tour_types = (types_basic | additional_types).distinct()
-        return Response(tour_types, status=200)
+        tour_types = set(list(types_basic) + list(additional_types))
+        return Response(list(tour_types), status=200)
