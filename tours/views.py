dc4b0d9e83c81da44d562dc3cb4cefb970ce9a2e
x3mart <x3mat@gmail.com>
2022-06-18 17:01:09 +0300

    tour filterset

--- tours/views.py
+++ tours/views.py
@@ -240,7 +240,7 @@ class FilterView(APIView):
         tour_types = TourType.objects.filter(Q(tours_by_basic_type__in=qs) | Q(tours_by_additional_types__in=qs)).order_by('name').values('name', 'id').distinct()      
         languages = Language.objects.filter(tours__in=qs).order_by('name').values('name', 'id').distinct()
         property_type = TourPropertyType.objects.filter(tours__in=qs).order_by('name').values('name', 'id').distinct()
-        accomodation = TourAccomodation.filter(tours__in=qs).order_by('name').values('name', 'id').distinct()
+        accomodation = TourAccomodation.objects.filter(tours__in=qs).order_by('name').values('name', 'id').distinct()
 
         filter_data = {
             'tour_types':tour_types,
