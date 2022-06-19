f2bc0d9a23a3a3f5c5d694a56d11eed5d22b33af
x3mart <x3mat@gmail.com>
2022-06-19 15:54:34 +0300

    order status

--- orders/serializers.py
+++ orders/serializers.py
@@ -147,4 +147,4 @@ class OrderListSerializer(serializers.ModelSerializer):
         if order.status == 'pending_prepayment':
             return f'{order.get_status_display()} {order.book_price}{order.currency} до {order.prepay_final_date.strftime("%d %B %Y") if order.prepay_final_date else ""}'
         if order.status == 'prepayment':
-            return f'{order.get_status_display()} постоплата {order.book_price}{order.currency}' + f' до {order.prepay_final_date.strftime("%d %B %Y")}' if order.postpay_final_date != order.start_date else 'в день старта'
+            return f'{order.get_status_display()} постоплата {order.book_price}{order.currency}' + f' до {order.postpay_final_date.strftime("%d %B %Y")}' if order.postpay_final_date != order.start_date else 'в день старта'
