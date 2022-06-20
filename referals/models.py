9330a6a578bd12d99cee50cd809a91f4564e4ff3
x3mart <x3mat@gmail.com>
2022-06-20 17:45:40 +0300

    referrals

--- referals/models.py
+++ referals/models.py
@@ -3,7 +3,7 @@ from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 class Referral(models.Model):
-    score = models.PositiveSmallIntegerField(_('Реферальные быллы'), default=1000)
+    score = models.PositiveSmallIntegerField(_('Реферальные быллы'), null=True, blank=True)
     referral = models.OneToOneField(('accounts.User'), on_delete=models.PROTECT, related_name='referral')
     beneficiary = models.ForeignKey(('accounts.User'), on_delete=models.PROTECT, related_name='beneficiary_referrals')
     tour = models.ForeignKey(('tours.Tour'), on_delete=models.PROTECT, related_name='tour_referrals')
