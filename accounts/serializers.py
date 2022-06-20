00dc33445606e9bcc0dd67752ddbea588c7a1213
x3mart <x3mat@gmail.com>
2022-06-20 19:04:43 +0300

    referrals

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -248,6 +248,7 @@ class CustomerSerializer(serializers.ModelSerializer):
         customer.save()
         if request.data.get('referral'):
             try:
+                print(utils.decode_uid(request.data.get('referral')))
                 beneficiary = User.objects.get(pk=utils.decode_uid(request.data.get('referral')))
                 Referral.objects.create(referral_id=customer.id, beneficiary_id=beneficiary.id)
                 Customer.objects.filter(pk=customer.id).update(referral_score=2200)
