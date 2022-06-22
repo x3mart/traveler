88071b6991e60873fde90fc3440ea627ca0ad4f3
x3mart <x3mat@gmail.com>
2022-06-22 22:08:10 +0300

    Tour filter

--- tours/views.py
+++ tours/views.py
@@ -247,20 +247,20 @@ class FilterView(APIView):
     def get(self, request, format=None):
         tour_basic = TourBasic.objects.prefetch_related('expert')
         prefetch_tour_basic = Prefetch('tour_basic', tour_basic)
-        qs = Tour.objects.prefetch_related(prefetch_tour_basic, 'start_country', 'start_city', 'wallpaper', 'currency').only('id', 'name', 'start_date', 'start_country', 'start_city', 'price', 'discount', 'duration', 'tour_basic', 'wallpaper', 'vacants_number', 'currency').filter(is_active=True).filter(direct_link=False).filter(Q(booking_delay__lte=F('start_date') - datetime.today().date() - F('postpay_days_before_start')))
+        qs = super().get_queryset().prefetch_related(prefetch_tour_basic, 'start_country', 'start_city', 'wallpaper', 'currency').only('id', 'name', 'start_date', 'start_country', 'start_city', 'price', 'discount', 'duration', 'tour_basic', 'wallpaper', 'vacants_number', 'currency').filter(is_active=True).filter(direct_link=False).filter(Q(booking_delay__lte=F('start_date') - datetime.today().date() - F('postpay_days_before_start')))
         tour_types = TourType.objects.filter(Q(tours_by_basic_type__in=qs) | Q(tours_by_additional_types__in=qs)).order_by('name').values('name', 'id').distinct()      
         languages = Language.objects.filter(tours__in=qs).order_by('name').values('name', 'id').distinct()
         property_type = TourPropertyType.objects.filter(tours__in=qs).order_by('name').values('name', 'id').distinct()
         accomodation = TourAccomodation.objects.filter(tours__in=qs).order_by('name').values('name', 'id').distinct()
 
         filter_data = [
-            {'title': 'Типы туров', 'type':'tour_type', 'data': tour_types},
+            {'title': 'Типы туров', 'type':'tour_types', 'data': tour_types},
             {'title': 'Языки тура', 'type':'languages', 'data': languages},
             {'title': 'Проживание', 'type':'property_type', 'data': property_type},
             {'title': 'Размещение', 'type':'accomodation', 'data': accomodation}
         ]
             
-        return Response(FilterSerializer(filter_data).data, status=200)
+        return Response(filter_data, status=200)
 
 class ActiveRegions(APIView):
     def get(self, request, format=None):
