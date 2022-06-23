20737ff07914130beeede906d52a52bca6b85a2f
x3mart <x3mat@gmail.com>
2022-06-23 13:49:25 +0300

    Order pagination

--- orders/paginations.py
+++ orders/paginations.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from orders.models import Order
 
 class OrderResultsSetPagination(PageNumberPagination):
-    page_size = 50
+    page_size = 3
     page_size_query_param = 'page_size'
     max_page_size = 1000
 
