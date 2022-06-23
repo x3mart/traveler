88591fadaa9f647eab1615311519edf4a71c121d
x3mart <x3mat@gmail.com>
2022-06-23 15:03:27 +0300

    Tour pagination

--- tours/paginations.py
+++ tours/paginations.py
@@ -28,10 +28,10 @@ class TourResultsSetPagination(PageNumberPagination):
     
     def get_filter_data(self, queryset, request):
         tour_ids = queryset.values_list('id', flat=True)
-        tour_types = TourType.objects.filter(Q(tours_by_basic_type__in=queryset) | Q(tours_by_additional_types__in=queryset)).order_by('name').values('name', 'id').distinct()      
-        languages = Language.objects.filter(tours_id__in=tour_ids).order_by('name').values('name', 'id').distinct()
-        property_type = TourPropertyType.objects.filter(tours_id__in=tour_ids).order_by('name').values('name', 'id').distinct()
-        accomodation = TourAccomodation.objects.filter(tours_id__in=tour_ids).order_by('name').values('name', 'id').distinct()
+        tour_types = TourType.objects.filter(Q(tour_id__in=queryset)).order_by('name').values('name', 'id').distinct()      
+        languages = Language.objects.filter(tour_id__in=tour_ids).order_by('name').values('name', 'id').distinct()
+        property_type = TourPropertyType.objects.filter(tour_id__in=tour_ids).order_by('name').values('name', 'id').distinct()
+        accomodation = TourAccomodation.objects.filter(tour_id__in=tour_ids).order_by('name').values('name', 'id').distinct()
 
         filter_data = [
             {'title': 'Типы туров', 'type':'tour_types', 'data': tour_types},
