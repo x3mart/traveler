49127b0c2aa76460f954c473242cf523d70c8bd0
x3mart <x3mat@gmail.com>
2022-06-27 20:12:15 +0300

    tour filters

--- tours/paginations.py
+++ tours/paginations.py
@@ -32,7 +32,7 @@ class TourResultsSetPagination(PageNumberPagination):
         filter_set = {}
         for filter in filters:
             if filter != field:
-                filter_set.upgrade(filters[filter])
+                filter_set.update(filters[filter])
         return filter_set
     
     def get_filter_data(self, queryset, request):
@@ -50,16 +50,16 @@ class TourResultsSetPagination(PageNumberPagination):
         for type in params:
             if type == 'price':
                 value = params[type][0].split(',')
-                filters.upgrade({'price':{'discounted_price__gte':value[0], 'discounted_price__lte':value[1]}})
+                filters.update({'price':{'discounted_price__gte':value[0], 'discounted_price__lte':value[1]}})
             elif type == 'price':
                 value = params[type][0].split(',')
-                filters.upgrade({'languages':{'languages_in':value}})
+                filters.update({'languages':{'languages_in':value}})
             elif type == 'age':
                 value = params[type][0].split(',')
-                filters.upgrade({'age':{'age_starts__lte':value[0], 'age_ends__gte':value[1]}})
+                filters.update({'age':{'age_starts__lte':value[0], 'age_ends__gte':value[1]}})
             elif type == 'duration':
                 value = params[type][0].split(',')
-                filters.upgrade({'duration':{'duration__gte':value[0], 'duration__lte':value[1]}})
+                filters.update({'duration':{'duration__gte':value[0], 'duration__lte':value[1]}})
                 
         qs_tour_types = active_tours.filter(**self.get_field_filter(filters, 'tour_types'))
         tour_types = TourType.objects.filter(Q(tours_by_basic_type__in=qs_tour_types) | Q(tours_by_additional_types__in=qs_tour_types)).order_by('name').values('name', 'id').distinct()
