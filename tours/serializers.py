7863d8442038b4a8aef7836ecd45b2db757f2efa
x3mart <x3mat@gmail.com>
2022-07-05 14:01:25 +0300

    tour list seializer public & api urls

--- tours/serializers.py
+++ tours/serializers.py
@@ -207,6 +207,8 @@ class TourListSerializer(serializers.ModelSerializer, TourSerializerMixin):
         return None
     
     def get_public_url(self, obj):
+        print(obj)
+        print(obj.slug)
         if obj.start_region.slug == 'rossiia':
             return f'{obj.start_region.slug}/{obj.start_russian_region.slug}/{obj.slug}/?date_id={obj.id}'
         return f'{obj.start_region.slug}/{obj.start_country.slug}/{obj.slug}/?date_id={obj.id}'
