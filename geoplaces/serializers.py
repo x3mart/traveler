2331da35300a4eabdf75074da4ebb2db30e23871
x3mart <x3mat@gmail.com>
2022-07-06 17:59:46 +0300

    tour destination

--- geoplaces/serializers.py
+++ geoplaces/serializers.py
@@ -69,7 +69,14 @@ class RegionShortSerializer(serializers.ModelSerializer):
 
 
 class DestinationSerializer(serializers.ModelSerializer):
-
+    name = serializers.SerializerMethodField(read_only=True)
+    public_url = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = Destination
         fields = '__all__'
+    
+    def get_name(self, obj):
+        return obj.country.name if obj.country else obj.country_region.name
+    
+    def public_url(self, obj):
+        return f'{obj.country.region.slug}/{obj.country.slug}' if obj.country else f'{obj.country_region.country.region.slug}/{obj.country_region.slug}'
