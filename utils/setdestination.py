3a1b36690062ea3e18761d6d68838c601312119e
x3mart <x3mat@gmail.com>
2022-07-06 16:43:10 +0300

    tour destination

--- utils/setdestination.py
+++ utils/setdestination.py
@@ -7,5 +7,7 @@ def set_destinations():
         Destination.objects.create(country_region_id=country_region_id)
     for country_id in Country.objects.exclude(name__in=['Россия', 'Россия2']).values_list('id', flat=True):
         Destination.objects.create(country_id=country_id)
+
+def set_tour_destinations()
     for tour in Tour.objects.exclude(start_city__isnull=True):
         tour.save()
