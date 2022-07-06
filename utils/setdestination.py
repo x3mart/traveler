5e6c16a63c1a7a464bbc756bae9b64b450a957c5
x3mart <x3mat@gmail.com>
2022-07-06 16:54:56 +0300

    tour destination

--- utils/setdestination.py
+++ utils/setdestination.py
@@ -3,7 +3,7 @@ from geoplaces.models import Country, CountryRegion, Destination
 from tours.models import Tour
 
 def set_destinations():
-    for country_region_id in CountryRegion.objects.filter(country__name__in=['Россия', 'Россия2']).values_list('id', flat=True):
+    for country_region_id in CountryRegion.objects.filter(country__name__in=['Россия']).values_list('id', flat=True):
         Destination.objects.create(country_region_id=country_region_id)
     for country_id in Country.objects.exclude(name__in=['Россия', 'Россия2']).values_list('id', flat=True):
         Destination.objects.create(country_id=country_id)
