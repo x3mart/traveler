dfc8cbb4988a0ceaea59ae05e72af998426fddb4
x3mart <x3mat@gmail.com>
2022-07-07 17:56:22 +0300

    country & russian regions to destination

--- geoplaces/models.py
+++ geoplaces/models.py
@@ -61,7 +61,7 @@ class City(models.Model):
     name = models.CharField(_('Название'), max_length=255)
     slug = models.SlugField(max_length = 255, null=True, blank=True)
     foreign_id = models.IntegerField(_('Сторонний ключ'), null=True, blank=True)
-    country = models.ForeignKey('Destination', on_delete=models.CASCADE, related_name='cities', verbose_name=_('Страна'), null=True, blank=True)
+    country = models.ForeignKey('Country', on_delete=models.CASCADE, related_name='cities', verbose_name=_('Страна'), null=True, blank=True)
     image = models.ImageField(_("Фото"), upload_to=geo_path, max_length=255, null=True, blank=True)
     alt =  models.CharField(_('alt текст'), max_length=255, null=True, blank=True)
 
