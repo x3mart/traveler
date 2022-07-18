3be16a5aa2dc8e433f57beeeb0d8fb73d8be5665
x3mart <x3mat@gmail.com>
2022-07-18 16:37:56 +0300

    traveler.yml

--- utils/retailrocket.py
+++ utils/retailrocket.py
@@ -1,5 +1,16 @@
 from django.template.loader import render_to_string
+from django.http import FileResponse
+from traveler.settings import BASE_DIR
+from geoplaces.models import Destination
+from tours.models import Tour
 
 
-def make_retailrocket_yml():
-    pass
+def make_retailrocket_yml(request):
+    tours = Tour.objects.in_sale().prefetched().with_discounted_price()
+    destinations = Destination.objects.filter(tours_by_start_destination__in=tours).distinct().prefetch_related('region').order_by('name').distinct()
+    content = render_to_string('traveler.yml', {'tours':tours, 'destinations':destinations})
+    print('WOW')
+    with open(f'{BASE_DIR}/media/traveler.yml', 'w', encoding='UTF-8') as static_file:
+        static_file.write(content)
+    print('WOW')
+    return FileResponse(open(f'{BASE_DIR}/media/traveler.yml'), as_attachment=True)
